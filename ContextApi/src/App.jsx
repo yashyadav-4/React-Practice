4e334b38f1c981e9ede2f2433fc635fdbea271bb
Yash Yadav <179575411+yashyadav-4@users.noreply.github.com>
2026-01-06 16:21:58 +0530

    updating minimal Changes

--- ContextApi/src/App.jsx
+++ ContextApi/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import UserContextProvider from './Context/UserContextProvider'
 import Profile from './components/Profile'
 import Login from './components/Login'
+import './App.css'
 
 function App() {
 
